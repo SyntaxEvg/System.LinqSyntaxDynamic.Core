c9aef288f0b95cba0e9938bf3e9878524c27bd0a
SyntaxEvgen <syntax-evgen@yandex.ru>
2023-06-20 19:19:25 +0300

    Добавьте файлы проекта.

---
+++ System.LinqSyntaxDynamic.Core/Parser/IConstantExpressionWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+
+namespace System.Linq.Dynamic.Core.Parser;
+
+internal interface IConstantExpressionWrapper
+{
+    void Wrap(ref Expression expression);
+
+    bool TryUnwrapAsValue<TValue>(MemberExpression? expression, out TValue? value);
+
+    bool TryUnwrapAsConstantExpression<TValue>(MemberExpression? expression, out ConstantExpression? value);
+}
